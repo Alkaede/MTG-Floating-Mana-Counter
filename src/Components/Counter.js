16da6d90574f13da2db2661f6225324dc302f138
Andrew Castillo <andrewcast32@gmail.com>
2021-10-10 01:28:59 -0700

    Redux setup

---
+++ src/Components/Counter.js
@@ -0,0 +1,7 @@
+// this file will be the skeleton of the counter, it will take information based on a JSON file and import the items through there
+
+const elementCounter = () => {
+  return "benis"
+}
+
+export default elementCounter;
