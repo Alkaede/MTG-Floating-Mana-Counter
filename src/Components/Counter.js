484725258b986cf0cba38f0729c396463a623031
Andrew Castillo <andrewcast32@gmail.com>
2021-10-14 16:10:30 -0700

    will have to readjust to redux soon, added json data

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,7 +1,34 @@
 // this file will be the skeleton of the counter, it will take information based on a JSON file and import the items through there
+import React, {useState} from 'react';
+import mana from '../mana.json';
 
-const elementCounter = () => {
-  return "benis"
+const ElementCounter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const onClick = () =>{
+    setCounter(counter + 1);
+  }
+
+  const reset = () => {
+    setCounter(0)
+  }
+
+  console.log(mana)
+  return (
+    <div>
+      {mana &&
+        mana.map(el => {
+          return <img src={el.image} onClick={onClick} alt={`mana color: ${el.mana}`}/>
+        })
+      }
+      <h1>
+      {counter}
+      </h1>
+      <button onClick={reset}>
+      benis reset
+      </button>
+    </div>
+  );
 }
 
-export default elementCounter;
+export default ElementCounter;
